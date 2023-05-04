126034bbae0bd5ab2d546018d5ebf0079d444624
Simon <Simon-Dirks@users.noreply.github.com>
2023-05-04 11:56:59 +0200

    Build for esnext

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
